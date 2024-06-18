45fa26d343f958cab9db76a5db6fb1764e1202ca
tiendungdayyy <91593609+tiendungdayyy@users.noreply.github.com>
2024-06-18 16:03:53 +0700

    finish for publisher except logoImg

--- src/pages/admin/Publisher.jsx
+++ src/pages/admin/Publisher.jsx
@@ -1,22 +1,195 @@
-// material-ui
-import Typography from '@mui/material/Typography';
-
 // project import
 import MainCard from 'components/MainCard';
+import productServices from 'services/productServices';
+import { Component } from "react";
+import ModalCreatePublisher from 'modals/ModalCreatePublisher';
+import ModalUpdatePublisher from 'modals/ModalUpdatePublisher';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { emitter } from 'utils/emitter';
+
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
-const Publisher = () => {
-    return (
-        <MainCard title="Sample Card">
-            <Typography variant="body2">
-                Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-                minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-                reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui
-                officiate descent molls anim id est labours.
-            </Typography>
-        </MainCard>
-    );
+class Publisher extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listPublisher: [],
+            isOpenedModalCreatePublisher: false,
+            isOpenedModalUpdatePublisher: false,
+            publisherUpdate: {},
+            publisherCreate: {},
+        }
+    }
+
+    toggleCreatePublisherModal = () => {
+        this.setState({
+            isOpenedModalCreatePublisher: !this.state.isOpenedModalCreatePublisher
+        });
+    }
+
+    toggleUpdatePublisherModal = () => {
+        this.setState({
+            isOpenedModalUpdatePublisher: !this.state.isOpenedModalUpdatePublisher
+        });
+    }
+
+
+    async componentDidMount() {
+        await this.getAllPublisher();
+    }
+
+    getAllPublisher = async () => {
+        let res = await productServices.getAllPublisher();
+        if (res) this.setState({ listPublisher: res.data })
+
+    }
+    // For add new publisher
+    handleAddNewPublisher = (Publisher) => {
+        this.setState({
+            isOpenedModalCreatePublisher: true,
+            PublisherCreate: Publisher
+        });
+    }
+
+    doCreatePublisher = async (Publisher) => {
+        try {
+
+            let res = await productServices.createPublisher(Publisher);
+            if (res) {
+                this.setState({
+                    isOpenedModalCreatePublisher: false,
+                    listPublisher: Publisher,
+                })
+                await this.getAllPublisher();
+                emitter.emit('EVENT_CLEAR_MODAL_DATA');
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    //For update publisher
+    handleUpdatePublisher = (publisher) => {
+        this.setState({
+            isOpenedModalUpdatePublisher: true,
+            publisherUpdate: publisher
+        });
+    }
+
+    doUpdatePublisher = async (publisher) => {
+        try {
+            let res = await productServices.updatePublisher(publisher);
+            if (res) {
+                this.setState({
+                    isOpenedModalUpdatePublisher: false,
+                    listPublisher: publisher,
+                })
+                await this.getAllPublisher();
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    // For delete Publisher
+    handleDeletePublisher = async (publisher) => {
+        try {
+            let res = await productServices.deletePublisher(publisher.id);
+            if (res) {
+
+                this.setState({
+                    isOpenedModalUpdatePublisher: false,
+                    listPublisher: publisher,
+                })
+
+                await this.getAllPublisher();
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    render() {
+
+        return (
+            <>
+                <MainCard>
+                    <div className='mx-1'>
+                        <button className='btn btn-primary px'
+                            style={{ backgroundColor: "green", marginBottom: 2 + "rem" }}
+                            onClick={this.handleAddNewPublisher}>
+                            <FontAwesomeIcon icon={faPlus} /> Thêm mới
+                        </button>
+                        {
+                            this.state.isOpenedModalCreatePublisher &&
+                            <ModalCreatePublisher
+                                open={this.state.isOpenedModalCreatePublisher}
+                                toggle={this.toggleCreatePublisherModal}
+                                publisherInfor={this.state.publisherCreate}
+                                createPublisher={this.doCreatePublisher}
+                            />
+                        }
+                        {
+                            this.state.isOpenedModalUpdatePublisher &&
+                            <ModalUpdatePublisher
+                                open={this.state.isOpenedModalUpdatePublisher}
+                                toggle={this.toggleUpdatePublisherModal}
+                                currentPublisher={this.state.publisherUpdate}
+                                updatePublisher={this.doUpdatePublisher}
+                            />
+                        }
+                    </div>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Tên sách</th>
+                                <th scope="col">Mô tả</th>
+                                <th scope="col">Trạng thái</th>
+                                <th scope="col">Thao tác</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.listPublisher &&
+                                    Array.isArray(this.state.listPublisher) ? this.state.listPublisher.map((data, i) => {
+                                        return (
+                                            <tr key={i}>
+                                                <td>{data.id}</td>
+                                                <td>{data.name}</td>
+                                                <td>{data.description}</td>
+                                                <td>{data.status}</td>
+
+
+                                                <td>
+                                                    <button type="button" className="btn btn-primary"
+                                                        onClick={() => this.handleUpdatePublisher(data)}>
+                                                        Cập nhật
+                                                    </button>
+                                                    <button type="button" className="btn btn-delete"
+                                                        style={{
+                                                            backgroundColor: "#FF4B4B",
+                                                            color: "white",
+                                                            marginLeft: 1 + "rem"
+                                                        }}
+                                                        onClick={() => { this.handleDeletePublisher(data) }}
+                                                    >
+                                                        Xóa
+                                                    </button>
+                                                </td >
+                                            </tr >
+                                        );
+                                    }) : null
+                            }
+                        </tbody >
+                    </table >
+                </MainCard>
+            </>
+        )
+    }
 }
 
 export default Publisher;
--- src/services/productServices.js
+++ src/services/productServices.js
@@ -2,35 +2,50 @@ import axios from "api/axios";
 
 //Category
 const createCategory = (createData) => {
-    return axios.post('/api/v1/admin/categories', createData);
+    return axios.post('/admin/api/v1/categories', createData);
 }
 
 const getAllCategory = () => {
-    return axios.get('/api/v1/admin/categories');
+    return axios.get('/admin/api/v1/categories');
 }
 
 const updateCategory = (updatedData) => {
-    return axios.put('/api/v1/admin/categories', updatedData);
+    return axios.put('/admin/api/v1/categories', updatedData);
 }
 
 const deleteCategory = (deletedId) => {
-    return axios.delete(`/api/v1/admin/categories/${deletedId}`);
+    return axios.delete(`/admin/api/v1/categories/${deletedId}`);
 }
 
 //Genre
 const getAllGenre = () => {
-    return axios.get('/api/v1/admin/genres');
+    return axios.get('/admin/api/v1/genres');
 }
 
 const createGenre = (createGenre) => {
-    return axios.post('/api/v1/admin/genres', createGenre);
+    return axios.post('/admin/api/v1/genres', createGenre);
 }
 
 const updateGenre = (updatedData) => {
-    return axios.put('/api/v1/admin/genres', updatedData);
+    return axios.put('/admin/api/v1/genres', updatedData);
 }
 const deleteGenre = (deletedId) => {
-    return axios.delete(`/api/v1/admin/genres/${deletedId}`);
+    return axios.delete(`/admin/api/v1/genres/${deletedId}`);
+}
+
+//Publisher
+const getAllPublisher = () => {
+    return axios.get('/admin/api/v1/publishers');
+}
+const createPublisher = (createPublisher) => {
+    return axios.post('/admin/api/v1/publishers', createPublisher);
+}
+
+const updatePublisher = (updatedData) => {
+    return axios.put('/admin/api/v1/publishers', updatedData);
+}
+const deletePublisher = (deletedId) => {
+    return axios.delete(`/admin/api/v1/publishers/${deletedId}`);
 }
 
 export default {
@@ -42,4 +57,8 @@ export default {
     createGenre,
     updateGenre,
     deleteGenre,
+    getAllPublisher,
+    createPublisher,
+    updatePublisher,
+    deletePublisher,
 };  
