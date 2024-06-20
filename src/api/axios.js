e41b3c3cd46bf31c9688b708e46a9778c3cec833
oned-vu <onedvu315itech@gmail.com>
2024-06-20 17:07:50 +0700

    change base URL for API

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'https://inkmelo-springboot-be.onrender.com'
+    baseURL: 'https://inkmelo-springboot-be-s2etd44lba-as.a.run.app'
 });
 
 export default api;
