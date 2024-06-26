b121c85cce0eb6111e18541f292e23c6ae71c8c0
oned-vu <onedvu315itech@gmail.com>
2024-06-27 00:03:02 +0700

    update route and navigate successfully when login

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,17 +2,10 @@ import axios from "api/axios";
 
 class AuthService {
     async login(username, password) {
-        return await axios.post('/api/v1/auth/sign-in', {
+        return await axios.post('/store/api/v1/auth/sign-in', {
             username,
             password,
-        })
-            .then((res) => {
-                if (res.data.accessToken) {
-                    localStorage.setItem("user", JSON.stringify(res.data));
-                }
-
-                return res.data;
-            });
+        });
     }
 
     logout() {
