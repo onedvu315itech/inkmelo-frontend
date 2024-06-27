d872b92e1eab709b4066d080502480b344e18188
tiendungdayyy <91593609+tiendungdayyy@users.noreply.github.com>
2024-06-27 08:00:04 +0700

    Update cartServices.js

--- src/services/cartServices.js
+++ src/services/cartServices.js
@@ -1,7 +1,7 @@
 import axios from 'api/axios'
 
 const getAllCart = async () => {
-    return axios.get(`/store/api/v1/cart-details?user=user1`)
+    return axios.get(`https://inkmelo-springboot-be-s2etd44lba-as.a.run.app/store/api/v1/customer/user1/cart-details`)
 }
 
 export default {
